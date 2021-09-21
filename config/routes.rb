41e66a749e1932c69b48dd5aaa2ef8700e39d680
ashraffares <ashraffares090@gmail.com>
2021-09-21 11:34:14 +0200

    Add resisteration route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/login', to: 'sessions#login'
+      post '/registeration', to: 'sessions#registeration'
       resources :todos do
         resources :items
       end
