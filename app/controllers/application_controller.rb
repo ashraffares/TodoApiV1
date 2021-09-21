78327dd2425436689ac121882428b7c9bfc91cf0
ashraffares <ashraffares090@gmail.com>
2021-09-21 12:57:32 +0200

    Define auth method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,21 @@
 class ApplicationController < ActionController::API
   SECRETS = 'my$ecretK3y'
 
-  def auth; end
+  def auth
+    token = request.headers.token
+    if token
+      decoded_token = decrypt(token)
+      user_id = decoded_token[0].user_id
+      user = User.find(user_id)
+      if user
+        true
+      else
+        render json: { error: 'Wrong token!' }
+      end
+    else
+      render json: { error: 'Missing token!' }
+    end
+  end
 
   def encrypt(payload)
     JWT.encode payload, SECRETS, 'HS256'
