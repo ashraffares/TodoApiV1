5796c52440a14e7195e457aacbe16da500e26ded
ashraffares <ashraffares090@gmail.com>
2021-09-21 11:32:35 +0200

    Add login route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/login', to: 'sessions#login'
       resources :todos do
         resources :items
       end
