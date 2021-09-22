24312ebee724ac0864774a70baa075a145baed51
ashraffares <ashraffares090@gmail.com>
2021-09-22 14:23:39 +0200

    fix line length error

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Todo, type: :model do
     it { should have_many(:items).dependent(:destroy) }
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:created_by) }
-    it { should validate_length_of(:title).is_at_least(3).is_at_most(25).with_message('it should be at least 3 character and maximum 25') }
+    it { should validate_length_of(:title)
+    .is_at_least(3).is_at_most(25).with_message('it should be at least 3 character and maximum 25') }
   end
 end
