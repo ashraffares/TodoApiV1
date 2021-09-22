57d3eb14f6ec65f401d22966b1868a9f83bbcd68
ashraffares <ashraffares090@gmail.com>
2021-09-22 12:22:20 +0200

    Add validation test for item todo models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :todo
   validates_presence_of :name, :proirity, :completed
-  validates :name, length: { minimum: 3, maximum: 25 }, message:
-  { msg: 'it should be at least 3 character and maximum 25' }
+  validates_length_of :name, minimum: 3, maximum: 25, message: 'it should be at least 3 character and maximum 25'
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:proirity) }
     it { should validate_presence_of(:completed) }
+    it { should validate_length_of(:name).is_at_least(3).with_message('it should be at least 3 character and maximum 25') }
   end
 end
