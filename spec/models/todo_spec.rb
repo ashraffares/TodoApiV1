a39af5acc7f914c1310ccb94459e61e78953e0cd
ashraffares <ashraffares090@gmail.com>
2021-09-22 14:29:27 +0200

    fix rubocop erros

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Layout/LineLength
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
@@ -5,7 +6,8 @@ RSpec.describe Todo, type: :model do
     it { should have_many(:items).dependent(:destroy) }
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:created_by) }
-    it { should validate_length_of(:title)
-    .is_at_least(3).is_at_most(25).with_message('it should be at least 3 character and maximum 25') }
+    it { should validate_length_of(:title).is_at_least(3).is_at_most(25).with_message('it should be at least 3 character and maximum 25') }
   end
 end
+
+# rubocop:enable
