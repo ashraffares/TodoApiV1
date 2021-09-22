1818ffbcfed80a33f8208707df900fdd547aad3a
ashraffares <ashraffares090@gmail.com>
2021-09-22 11:32:04 +0200

    Add test for user model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,5 @@
 class Todo < ApplicationRecord
   has_many :items, dependent: :destroy
   validates_presence_of :title, :created_by
-  validates :title, length: { minimum: 3, maximum: 25 }, message:
-  { msg: 'it should be at least 3 character and maximum 25' }
+  validates :title, length: { minimum: 3, maximum: 25, message: 'it should be at least 3 character and maximum 25'}
 end
