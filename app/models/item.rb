34f8e6a2d2168d6756afb6956029dd6f63f68f75
ashraffares <ashraffares090@gmail.com>
2021-09-22 11:08:48 +0200

    Add more validation to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :todo
   validates_presence_of :name, :proirity, :completed
+  validates :name, length: { minimum: 3, maximum: 25 }, message:
+  { msg: 'it should be at least 3 character and maximum 25' }
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ApplicationRecord
   has_many :items, dependent: :destroy
   validates_presence_of :title, :created_by
+  validates :title, length: { minimum: 3, maximum: 25 }, message:
+  { msg: 'it should be at least 3 character and maximum 25' }
 end
