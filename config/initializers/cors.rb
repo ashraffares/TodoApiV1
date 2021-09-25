4e6f1fb929da8b7d3390f27d0cb4de87d6a76a6b
ashraffares <ashraffares090@gmail.com>
2021-09-25 15:34:59 +0200

    Add methods any to allow any kind of action

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -20,6 +20,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any
+    resource '*', headers: :any, methods: :any
   end
 end
