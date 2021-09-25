6b5a3e35b50cd1bbc6df57f51db034acbb17bd0a
ashraffares <ashraffares090@gmail.com>
2021-09-25 13:00:03 +0200

    Create Assocation between user and todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,5 @@ class Todo < ApplicationRecord
   has_many :items, dependent: :destroy
   validates_presence_of :title, :created_by
   validates_length_of :title, minimum: 3, maximum: 25, message: 'it should be at least 3 character and maximum 25'
+  belong_to :user
 end
