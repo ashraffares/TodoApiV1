c30b00e83156e118bed3468c226249045484e880
ashraffares <ashraffares090@gmail.com>
2021-10-01 08:08:27 +0200

    remove unneccessary frozen string literal

--- db/migrate/20210925105858_add_user_id_to_todos.rb
+++ db/migrate/20210925105858_add_user_id_to_todos.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddUserIdToTodos < ActiveRecord::Migration[6.1]
   def change
     add_column :todos, :user_id, :integer
--- spec/requests/main_spec.rb
+++ spec/requests/main_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe 'Mains', type: :request do
