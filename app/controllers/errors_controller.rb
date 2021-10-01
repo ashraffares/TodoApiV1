7555c3c1515b7037c0b0838cf80330a30f6d55a7
ashraffares <ashraffares090@gmail.com>
2021-10-01 07:49:34 +0200

    Add status to return message 404 and 500

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@
 
 class ErrorsController < ApplicationController
   def not_found
-    render json: { errors: '404 Not Found Please make sure your in the right route' }
+    render json: { errors: '404 Not Found Please make sure your in the right route' }, status: 404
   end
 
   def internal_server_error
@@ -14,6 +14,6 @@ class ErrorsController < ApplicationController
           { LinkedIn: 'https://www.linkedin.com/in/faresashraf/' },
           { Github: 'https://github.com/ashraffares' },
           { Email: 'ashraffares090@gmail.com' }
-        ] }
+        ] }, status: 500
   end
 end
